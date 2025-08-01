bc00e57a30c0c65d07880157ea9b9fb9867b4c06
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-08-01 07:00:14 +0000

    game for fun

--- Dockerfile/game.cpp
+++ Dockerfile/game.cpp
@@ -7,45 +7,59 @@
 #include <algorithm>
 
 using namespace std;
-vector<string> word_list = {"python", "developer", "programming", "algorithm", "function", "randomfile"};
-string scramble_word(string word) {
+vector<string> word_list = {"python", "developer", "programming", "algorithm", 
+                            "function", "iterator", "codespace", "proccessor", 
+                            "keyboard", "javascript", "structure", "computer", 
+                            "software", "design", "coding", "website"};
+string scramble_word(string word)
+{
     random_shuffle(word.begin(), word.end());
     return word;
 }
-string play_game(string original_word, int attempts) {
+string play_game(string original_word, int attempts)
+{
     string guess;
     cout << "Guess the word (or type 'exit' to quit): ";
     cin >> guess;
-    if (guess == "exit") cout << "Thanks for playing! Goodbye!" << endl;
-    if(guess == original_word) cout << "Correct   You guessed it!" << "\n";
-    else cout << "Wrong guess. Attempts left: " << attempts << "\n";
+    if (guess == "exit")
+        cout << "Thanks for playing! Goodbye!" << endl;
+    if (guess == original_word)
+        cout << "Correct! You guessed it!" << "\n";
+    else
+        cout << "Wrong guess. Attempts left: " << attempts << "\n";
     return guess;
 }
 
-int main() {
+int main()
+{
     srand(time(0));
-    
-    while (true) {
+
+    while (true)
+    {
         string original_word = word_list[rand() % word_list.size()];
         string scrambled = scramble_word(original_word);
         cout << "\n Welcome to the Word Scramble Game!" << endl;
         cout << "Scrambled word: " << scrambled << endl;
-     int attempts = 3;
+        int attempts = 3;
 
-     while (attempts--) {
-        string guess = play_game(original_word, attempts);
-        if(guess == original_word || guess == "exit") break;
-}
-    cout << "The correct word was: " << original_word << "\n" << endl;
-     string play_again;
-     // Ask if the player wants to play again
-     cout << "Do you want to play again? (yes/no): ";
-     cin >> play_again;
-     if(play_again == "no") {
-        cout << "Thanks for playing! Goodbye!" << endl;
-        break;
-}
-     // write if condition to break out of loop user doesn't want to play
+        while (attempts--)
+        {
+            string guess = play_game(original_word, attempts);
+            if (guess == original_word || guess == "exit")
+                break;
+        }
+        cout << "The correct word was: " << original_word << "\n"
+             << endl;
+        string play_again;
+        // Ask if the player wants to play again
+        cout << "Do you want to play again? (yes/no): ";
+        cin >> play_again;
+        if (play_again == "no")
+        {
+            cout << "Thanks for playing! Goodbye!" << endl;
+            break;
+        }
+        // write if condition to break out of loop user doesn't want to play
     }
     return 0;
 }
